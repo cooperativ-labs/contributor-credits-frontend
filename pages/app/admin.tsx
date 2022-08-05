81d4bcec0175108f8c7ad278b81ff54944f6bf86
jchaselubitz <jchaselubitz@gmail.com>
2022-08-05 14:36:51 +0200

    make admin visible only to Jake

--- pages/app/admin.tsx
+++ pages/app/admin.tsx
@@ -5,6 +5,7 @@ import FormButton from '@src/Manager/components/buttons/FormButton';
 import PresentLegalText from '@src/Manager/components/PresentLegalText';
 import Input from '@src/Manager/forms/components/Inputs';
 import Select from '@src/Manager/forms/components/Select';
+import { WalletOwnerContext } from '@src/SetAppContext';
 import { ADD_DOCUMENT } from '@src/utils/dGraphQueries/agreement';
 import { MatchSupportedChains } from '@src/web3/connectors';
 import { Form, Formik } from 'formik';
@@ -13,35 +14,39 @@ import React from 'react';
 import { SmartContractType } from 'types/';
 
 const UpdateAgreementText: React.FC = () => {
-  const [updateAgreement, { data: agreementData, error: agreementError }] = useMutation(ADD_DOCUMENT);
-  return (
-    <>
-      <Formik
-        initialValues={{
-          agreementId: '',
-          agreementText: '',
-        }}
-        validate={(values) => {
-          const errors: any = {}; /** @TODO : Shape */
-        }}
-        onSubmit={async (values, { setSubmitting }) => {
-          await updateAgreement({
-            variables: { agreementId: values.agreementId, agreementText: values.agreementText },
-          });
-        }}
-      >
-        {({ values, isSubmitting }) => (
-          <Form className="flex flex-col gap relative">
-            <Input name="agreementId" className={''} />
-            <Input name="agreementText" className={''} />
-            <FormButton type="submit" disabled={isSubmitting}>
-              Add Agreement
-            </FormButton>
-            <PresentLegalText agreement={values.agreementText} />
-          </Form>
-        )}
-      </Formik>
-    </>
+  const { uuid } = React.useContext(WalletOwnerContext);
+  const [updateAgreement, { data, error }] = useMutation(ADD_DOCUMENT);
+
+  console.log(data, error);
+
+  return uuid === '0x531518985601FE8866fd5F39e9a3754F1fc38296' ? (
+    <Formik
+      initialValues={{
+        agreementId: '',
+        agreementText: '',
+      }}
+      validate={(values) => {
+        const errors: any = {}; /** @TODO : Shape */
+      }}
+      onSubmit={async (values, { setSubmitting }) => {
+        await updateAgreement({
+          variables: { agreementId: values.agreementId, agreementText: values.agreementText },
+        });
+      }}
+    >
+      {({ values, isSubmitting }) => (
+        <Form className="flex flex-col gap relative">
+          <Input name="agreementId" className={''} />
+          <Input name="agreementText" className={''} />
+          <FormButton type="submit" disabled={isSubmitting}>
+            Add Agreement
+          </FormButton>
+          <PresentLegalText agreement={values.agreementText} />
+        </Form>
+      )}
+    </Formik>
+  ) : (
+    <></>
   );
 };
 
