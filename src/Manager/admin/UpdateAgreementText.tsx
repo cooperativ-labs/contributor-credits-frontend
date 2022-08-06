94bd8352633561f733e3226b17a052c908efd304
jchaselubitz <jchaselubitz@gmail.com>
2022-08-06 14:27:51 +0200

    clean up admin add contract text

--- src/Manager/admin/UpdateAgreementText.tsx
+++ src/Manager/admin/UpdateAgreementText.tsx
@@ -1,15 +1,9 @@
 import { useMutation } from '@apollo/client';
-import { stringLength } from '@firebase/util';
-import { LoadingButtonText } from '@src/Manager/components/buttons/Button';
 import FormButton from '@src/Manager/components/buttons/FormButton';
 import PresentLegalText from '@src/Manager/components/PresentLegalText';
 import Input from '@src/Manager/forms/components/Inputs';
-import Select from '@src/Manager/forms/components/Select';
-import { WalletOwnerContext } from '@src/SetAppContext';
 import { ADD_DOCUMENT } from '@src/utils/dGraphQueries/agreement';
-import { MatchSupportedChains } from '@src/web3/connectors';
 import { Form, Formik } from 'formik';
-import { ValuesOfCorrectTypeRule } from 'graphql';
 import React from 'react';
 
 const UpdateAgreementText: React.FC = () => {
@@ -33,8 +27,8 @@ const UpdateAgreementText: React.FC = () => {
     >
       {({ values, isSubmitting }) => (
         <Form className="flex flex-col gap relative">
-          <Input name="agreementId" className={''} />
-          <Input textArea name="agreementText" className={''} />
+          <Input name="agreementId" className={''} labelText="Agreement ID" />
+          <Input textArea name="agreementText" className={''} labelText="Agreement Text" />
           <FormButton type="submit" disabled={isSubmitting}>
             Add Agreement
           </FormButton>
